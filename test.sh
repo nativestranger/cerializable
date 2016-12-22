922a1bba358b34ae7e7c42c40988e7fa15273cda
Eric Arnold <eric.ed.arnold@gmail.com>
2016-12-22 01:36:56 -0500

    appraise with rubies 2.2.6 and 2.3.3

--- test.sh
+++ test.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-rubies=("ruby-2.2.2" "ruby-2.3.1")
+rubies=("ruby-2.2.6" "ruby-2.3.3")
 for i in "${rubies[@]}"
 do
   echo "====================================================="
   echo "$i: Start Test"
   echo "====================================================="
+  rvm $i exec gem install bundler
   rvm $i exec bundle install
   rvm $i exec appraisal install
   rvm $i exec appraisal rake test
